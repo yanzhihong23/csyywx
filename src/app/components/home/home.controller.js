f82e2bb8efdad2ccf6412f55582aa3270252a492
duxiaodong <duxiaodng@darlin.me>
2015-06-04 20:19:10 +0800

    home

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('csyywx')
-	.controller('HomeCtrl', function($scope) {
-		$scope.banners = [{
-			src: 'img/ba'
-		}];
-	})
+    .controller('HomeCtrl', function ($scope) {
+        $scope.banners = [{
+            href: '',
+            src: 'assets/images/banner001.jpg',
+            alt: ''
+        },
+        {
+            href: 'http://www.nonobank.com',
+            src: 'assets/images/banner002.jpg',
+            alt: ''
+        }];
+    })
+;
