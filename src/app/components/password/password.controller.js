2136a9a761e1139f232eff4fe3147fed7b14b787
duxiaodong <duxiaodng@darlin.me>
2015-06-09 19:36:14 +0800

    setting salary and keyboard

--- src/app/components/password/password.controller.js
+++ src/app/components/password/password.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('csyywx')
-	.controller('PasswordCtrl', function() {
-		
+	.controller('PasswordCtrl', function($scope) {
+
+		$scope.enter = function(num) {
+			if (num===-1) {
+				console.log("===clear===");
+			} else {
+				console.log("===number %d entered", num);
+			}
+		}
+
 	})
