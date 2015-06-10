34e68eea30e013d29ed5250fac73bfe5cf2c1b61
Hom <yanzhihong23@qq.com>
2015-06-10 17:16:40 +0800

    refactor buy controller

--- src/app/components/buy/buy.controller.js
+++ src/app/components/buy/buy.controller.js
@@ -2,211 +2,106 @@
 
 angular.module('csyywx')
 	.controller('BuyCtrl', function($scope, $ionicLoading, UserApi, PayApi, userConfig) {
+		var sessionId = userConfig.getSessionId(), productCode, level;
 
-		$scope.api = {
-			sessionId: userConfig.getSessionId()
+		$scope.salaryDaySet = false;
+
+		$scope.order = {
+			amount: null,
+			range: 45.82,
+			reward: 0
 		};
+		// init
+		$ionicLoading.show();
+		PayApi.getProductCode({sessionId: ''})
+			.success(function(data) {
+				$ionicLoading.hide();
+				if (+data.flag === 1) {
+					productCode = data.data.productCode;
+					$ionicLoading.show();
+					PayApi.initOrder({
+						sessionId: userConfig.getSessionId(),
+						productCode: productCode
+					}).success(function(data) {
+							$ionicLoading.hide();
+							if (+data.flag === 1) {
+								initRange(data.data.monthRates);
+								$scope.salaryDaySet = data.data.isSetSalaryDay;
+							}
+						})
+					;
+				}
+			});
 
-		// 需要提交的表单数据
-		$scope.form = {
-			term: 10, // 期限
-			increaseRate: 2, // 加息
-			amount: '' // 投资金额
-		};
+		var initRange = function(data) {
+			$scope.terms = data.map(function(obj) {
+				return {
+					name: obj.monthName,
+					annualYield: (obj.productRate*100 + obj.monthRate*100 + obj.increaseRate*100)/100,
+					minAmount: 1,
+					maxAmount: +obj.investAmountMax,
+					days: +obj.countDays,
+					desc: obj.description,
+					systemMonthRateId: obj.systemMonthRateId
+				}
+			});
 
-		// 其他数据
-		$scope.other = {
-			interest: 0.00, // 基本收益率
-			totalInterest: 0.00, // 百分数
-			level: 1, // 期限层级
-			minAmount: 10, // 最小投资金额
-			maxAmount: 200000, // 最大投资金额
-			left: 0, // 悬浮滑块移动距离
-			isSetSalaryDay: 0, // 是否设置过加息日
-			isBetweenSalaryDate: 1, // 当前日期是否在加息日中
-			back: { // 到期可收回
-				total: 0.00,
-				int: '0', // 整数部分
-				dec: '.00元' // 小数部分
-			}
-		};
+			$scope.$watch('order.range', function(val) {
+				var len = $scope.terms.length;
+				$scope.order.left = val*1.83 + '%';
+				level = (Math.abs(val-100/len/2)/100*len).toFixed(0);
 
-		// 滑块数据
-		$scope.range = {
-			terms: []
-		};
+				if(!$scope.terms.length) return;
 
-		//
-		var Func = {
-
-			changeInterest: function() {
-				var level = $scope.other.level;
-				var terms = $scope.range.terms;
-				$scope.form.increaseRate = terms[level].increaseRate;
-				$scope.other.interest = terms[level].interest;
-				$scope.other.totalInterest = ($scope.other.interest + $scope.form.increaseRate).toFixed(2);
-			},
-
-			// 更改期限
-			changeTerm: function() {
-				var level = $scope.other.level;
-				var terms = $scope.range.terms;
-				terms.forEach(function(self) {
+				$scope.terms.forEach(function(self) {
 					if (self.active) {
 						self.active = false;
 					}
 				});
 
-				terms[level].active = true;
-			},
+				$scope.terms[level].active = true;
 
-			// 更改滑块移动距离
-			changeLeft: function() {
-				var term = $scope.form.term;
-				$scope.other.left = term*1.83 + '%';
-			},
+				var term = $scope.terms[level];
+				$scope.order.name = term.name;
+				$scope.order.annualYield = term.annualYield;
+				$scope.order.maxAmount = term.maxAmount;
+				$scope.order.days = term.days;
+				$scope.order.desc = term.desc;
+				$scope.order.systemMonthRateId = term.systemMonthRateId;
 
-			// 更改最大投资金额
-			changeMaxAmount: function() {
-				var level = $scope.other.level;
-				var terms = $scope.range.terms;
-				$scope.other.maxAmount = terms[level].maxAmount;
-			},
+				calculate();
+			});
 
-			// 计算到期可收回
-			changeBack: function() {
-				var level = $scope.other.level;
-				var terms = $scope.range.terms;
-				$scope.other.back.total = $scope.form.amount * ($scope.other.interest + $scope.form.increaseRate)/100/365 * terms[level].days;
-				//if (isNaN($scope.other.back.total)) {
-				//	$scope.other.back.int = '金额超过上限';
-				//	$scope.other.back.dec = '';
-				//} else {
-				//	$scope.other.back.int = Math.floor($scope.other.back.total);
-				//	$scope.other.back.dec = '.' + (100*($scope.other.back.total.toFixed(2) - $scope.other.back.int)).toFixed(0) + '元';
-				//}
-			},
-
-			checkAmount: function() {
-
-				$scope.form.amount = parseFloat($scope.form.amount);
-				var dec = ($scope.form.amount+'').split('.')[1];
-				if (dec && dec.length > 2) {
-					$scope.form.amount = Math.floor(100*$scope.form.amount)/100;
-				}
-
-				if ($scope.form.amount > $scope.other.maxAmount) {
-					$scope.form.amount = $scope.other.maxAmount;
-				}
-			},
-
-			// api返回数据
-			// 期限利率对象 monthRates
-			monthRates: function(data) {
-				data.forEach(function(self) {
-					var each = {};
-					each.term = self.monthName;
-					each.interest = self.productRate;
-					each.maxAmount = self.investAmountMax;
-					each.days = self.countDays;
-					each.increaseRate = self.increaseRate;
-
-					$scope.range.terms.push(each);
-				});
-			},
-			// 返回其他数据
-			otherData: function(data) {
-				$scope.other.minAmount = +data.productRule.investAmountMin;
-				$scope.other.isSetSalaryDay = data.isSetSalaryDay;
-				$scope.other.isBetweenSalaryDate = data.isBetweenSalaryDate;
-			},
-
-			watches: function() {
-				$scope.$watch('form.term', function() {
-					var len = $scope.range.terms.length;
-					var term = $scope.form.term;
-					if (len === 0) return;
-					$scope.other.level = (Math.abs(term-100/len/2)/100*len).toFixed(0);
-
-					Func.changeLeft();
-				});
-				$scope.$watch('other.level', function() {
-					var level = $scope.other.level;
-					var terms = $scope.range.terms;
-					if (!terms[level]) return;
-					Func.changeTerm();
-					Func.changeInterest();
-					Func.changeMaxAmount();
-					Func.changeBack();
-				});
-
-				$scope.$watch('form.amount', function() {
-					if ($scope.form.amount === '') {
-						return;
-					}
-					if (/\.$/.test($scope.form.amount)) {
-						$scope.form.amount.replace(/\.+$/, '.');
-						return;
-					}
-
-					Func.changeBack();
-
-					Func.checkAmount();
-				});
-			},
-
-			others: function() {
-				$scope.inputBlur = function() {
-					if ($scope.form.amount < $scope.other.minAmount && $scope.form.amount !== '') {
-						$scope.form.amount = $scope.other.minAmount;
-					}
-				};
-
-				window.addEventListener('touchend', function() {
-					//	停止滑动
-					var len = $scope.range.terms.length;
-					var level = $scope.other.level;
-					if (len === 0) return;
-					$scope.form.term = level/len*100 + 100/len/2;
-					if (+level === 0) {
-						$scope.form.term = 1;
-					}
-					if (+level === len -1) {
-						$scope.form.term = 99;
-					}
-					$scope.$apply();
-				});
-
-				window.addEventListener('touchstart', function() {
-					//	滑块bug，投资金额焦点导致滑块不能滑动
-					document.getElementById('amount').blur();
-				});
-			}
-
+			$scope.$watch('order.amount', function(val) {
+				calculate();
+			});
 		};
 
-		$ionicLoading.show();
-		PayApi.getProductCode({sessionId: ''})
-			.success(function(data) {
-				console.log(data);
-				$ionicLoading.hide();
-				if (+data.flag === 1) {
-					$scope.api.productCode = data.data.productCode;
-					$ionicLoading.show();
-					PayApi.initOrder($scope.api)
-						.success(function(data) {
-							console.log(data);
-							$ionicLoading.hide();
-							if (+data.flag === 1) {
-								Func.monthRates(data.data.monthRates);
-								Func.otherData(data.data);
-								Func.watches();
-								Func.others();
-							}
-						})
-					;
-				}
-			})
-		;
+		var calculate = function() {
+			$scope.order.amount = Math.min(parseFloat($scope.order.amount), $scope.order.maxAmount);
+			$scope.order.reward = +$scope.order.amount*$scope.order.annualYield/100/365*$scope.order.days || 0;
+		};
+
+		window.addEventListener('touchend', function() {
+			var len = $scope.terms.length;
+			if (len === 0) return;
+			$scope.order.range = level/len*100 + 100/len/2;
+			if (+level === 0) {
+				$scope.order.range = 1;
+			}
+			if (+level === len -1) {
+				$scope.order.range = 99;
+			}
+			$scope.$apply();
+		});
+
+		window.addEventListener('touchstart', function() {
+			//	滑块bug，投资金额焦点导致滑块不能滑动
+			document.getElementById('amount').blur();
+		});
+
+		$scope.submit = function() {
+			console.log($scope.order);
+		};
 
 	});
