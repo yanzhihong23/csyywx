1605d65286f3fe134cfd14f32e2fb873ec2413ed
Hom <yanzhihong23@qq.com>
2015-06-11 11:01:57 +0800

    add keyboard and pay password directives

--- src/app/components/quickpay/quickpay.controller.js
+++ src/app/components/quickpay/quickpay.controller.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('csyywx')
-	.controller('QuickpayCtrl', function($scope, orderService) {
+	.controller('QuickpayCtrl', function($scope, $rootScope, orderService) {
 		$scope.order = orderService.order;
 		console.log(orderService.order);
+
+		$rootScope.$on('payPassword', function(evt, password) {
+			console.log(password);
+			$rootScope.$broadcast('resetPayPassword');
+		})
 	})
---
+++ src/app/services/directives.js
@@ -0,0 +1,112 @@
+'use strict';
+
+angular.module('csyywx')
+	.directive('keyboard', function($rootScope) {
+		return {
+			restrict: 'E',
+			templateUrl: 'app/templates/keyboard.html',
+			link: function(scope, element, attr) {
+				angular.element(element).parent().addClass('has-keyboard');
+				scope.enter = function(num) {
+					$rootScope.$broadcast('keyboard', num);
+				};
+			}
+		}
+	})
+	.directive('payPassword', function($rootScope, $timeout) {
+		return {
+			restrict: 'E',
+			// require: 'ngModel',
+			templateUrl: 'app/templates/pay-password.html',
+			link: function(scope, element, attr) {
+				scope.inserts = [];
+				for (var i=0; i<6; i++) {
+					var insert = {
+						show: false,
+						number: ''
+					};
+					scope.inserts.push(insert);
+				}
+
+				var insertPassword = (function() {
+					scope.ok = false;
+					var current = 0;
+					var len = scope.inserts.length;
+					var timer;
+
+					function getPassword() {
+						var p = '';
+						for(var i=0; i<len; i++) {
+							p += scope.inserts[i].number;
+						}
+
+						return p;
+					}
+
+					function deleteN(curr) {
+						console.log('current', current);
+						if (timer) {
+							$timeout.cancel(timer);
+						}
+
+						scope.ok = false;
+
+						curr.show = false;
+						curr.number = '';
+
+						current--;
+					}
+
+					function addN(curr, num) {
+						scope.ok = false;
+
+						timer = $timeout(function() {
+							curr.show = true;
+						}, 300);
+						curr.number = num;
+
+						current++;
+					}
+
+					function reset() {
+						scope.inserts.forEach(function(self) {
+							deleteN(self);
+						});
+					}
+
+					function insertNumber(num, callback) {
+						if (num === -1) { // delete a number
+							if (current) {
+								deleteN(scope.inserts[current-1]);
+							}
+						} else if(num === 'ok') {
+							$rootScope.$broadcast('payPassword', getPassword());
+						} else {
+							if (current < len) {
+								addN(scope.inserts[current], num);
+								if (current === len) {
+									scope.ok = true;
+									if (callback) callback();
+								}
+							}
+						}
+
+					}
+
+					return {
+						insertNumber: insertNumber,
+						reset: reset
+					}
+
+				})();
+
+				$rootScope.$on('keyboard', function(evt, num) {
+					insertPassword.insertNumber(num);
+				});
+
+				$rootScope.$on('resetPayPassword', function() {
+					insertPassword.reset();
+				})
+			}
+		}
+	})
