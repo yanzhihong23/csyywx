66e146dec492f164d28d2a40201253f7cef0a96b
Hom <yanzhihong23@qq.com>
2015-06-17 18:33:10 +0800

    tabs init

--- src/app/tabs/tabs.controller.js
+++ src/app/tabs/tabs.controller.js
@@ -2,7 +2,15 @@
 
 angular.module('csyywx')
   .controller('TabsCtrl', function ($scope, $rootScope, $state) {
+		var reset = function() {
+    	$rootScope.hideTabs = !/tabs.home|tabs.info|tabs.buy/.test($state.current.name);
+    };
+
     $rootScope.$on('$ionicView.beforeEnter', function() {
-      $rootScope.hideTabs = !/tabs.home|tabs.info|tabs.buy/.test($state.current.name);
+      reset();
+    });
+
+    $rootScope.$on('$ionicView.afterEnter', function() {
+      reset();
     });
   });
